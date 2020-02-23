4131299c3ccbb3ad851c037651f39c7bebe62139
mxins <mxins@163.com>
2020-02-23 15:27:30 +0800

    git

--- install-v2ray.sh
+++ install-v2ray.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-yum install curl vim -y
+yum install curl vim git net-tools -y
+git clone https://github.com/mxins/my-v2ray.git
+cd my-v2ray
 bash go.sh
 systemctl start v2ray
 systemctl status v2ray
 systemctl stop firewalld
 systemctl disable firewalld
 
-#:<<config
 
-#config
 
